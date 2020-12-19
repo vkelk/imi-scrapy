9fc8847a083687aceeb4c91d0813f9cd2ee7b146
Vasko Kelkocev <pisimina@gmail.com>
2020-12-19 14:41:02 +0100

    add analysis

---
+++ imi/analyze.py
@@ -0,0 +1,93 @@
+import logging
+import os
+import re
+
+from sqlalchemy import or_
+
+from imi.models import Session, ImiTexts, Analysis, SentimentHarvard
+from .Generic_Parser import get_data
+from .Load_MasterDictionary import load_masterdictionary
+
+
+HIV4_DICTIONARY_FILE = 'HIV-4.csv'
+LM_DICTIONARY_FILE = 'LoughranMcDonald_MasterDictionary_2016.csv'
+
+logger = logging.getLogger(__name__)
+hiv_dictionary_path = os.path.join('dictionaries', HIV4_DICTIONARY_FILE)
+hiv4_dictionary = load_masterdictionary(hiv_dictionary_path, True)
+
+
+def analyze_artices():
+    logger.info('*** Analyzing Started...')
+    session = Session()
+    articles = session.query(ImiTexts.gan, ImiTexts.text) \
+        .outerjoin(Analysis, Analysis.gan == ImiTexts.gan)\
+        .outerjoin(SentimentHarvard, SentimentHarvard.gan == ImiTexts.gan)\
+        .filter(or_(Analysis.gan.is_(None), SentimentHarvard.gan.is_(None))).all()
+    i = 0
+    for article in articles:
+        text = article.text.rstrip('None')
+        # Loughran-McDonald
+        analysed = session.query(Analysis).filter_by(id=article.gan).first()
+        if analysed is None:
+            logger.info('Analysing Loughran-McDonald for article id %s', article.gan)
+            lm_dict = get_data(text)
+            lm_dict['id'] = article.gan
+            lm_dict.pop('doc_size')
+            a = Analysis(**lm_dict)
+            session.add(a)
+        # Harvard IV-4
+        sentiment_harvard = session.query(SentimentHarvard).filter_by(article_id=article.gan).first()
+        if sentiment_harvard is None:
+            logger.info('Analysing Harvard Inquirer Categories for article id %s', article.gan)
+            hiv4_dict = analyze_harvard_iv4(text)
+            hiv4_dict['article_id'] = article.gan
+            h = SentimentHarvard(**hiv4_dict)
+            session.add(h)
+        session.commit()
+    logger.info('*** Analyzing Finished...')
+
+
+def analyze_harvard_iv4(text):
+    COLUMNS = [
+        'article_id', 'word_count', 'positiv', 'negativ', 'pstv', 'affil', 'ngtv',
+        'hostile', 'strong', 'power', 'weak', 'submit', 'active', 'passive'
+        ]
+    vdictionary = {}
+    _odata = [0] * 15
+    total_syllables = 0
+    word_length = 0
+    tokens = re.findall('\w+', text)
+    for token in tokens:
+        token = token.upper()
+        if not token.isdigit() and len(token) > 1 and token in hiv4_dictionary:
+            
+            _odata[1] += 1  # word count
+            word_length += len(token)
+            if token not in vdictionary:
+                vdictionary[token] = 1
+            if hiv4_dictionary[token].positiv: _odata[2] += 1
+            if hiv4_dictionary[token].negativ: _odata[3] += 1
+            if hiv4_dictionary[token].pstv: _odata[4] += 1
+            if hiv4_dictionary[token].affil: _odata[5] += 1
+            if hiv4_dictionary[token].ngtv: _odata[6] += 1
+            if hiv4_dictionary[token].hostile: _odata[7] += 1
+            if hiv4_dictionary[token].strong: _odata[8] += 1
+            if hiv4_dictionary[token].power: _odata[9] += 1
+            if hiv4_dictionary[token].weak: _odata[10] += 1
+            if hiv4_dictionary[token].submit: _odata[11] += 1
+            if hiv4_dictionary[token].active: _odata[12] += 1
+            if hiv4_dictionary[token].passive: _odata[13] += 1
+
+    # Convert counts to %
+    try:
+        for i in range(2, 13 + 1):
+            _odata[i] = (_odata[i] / _odata[1]) * 100
+    except ZeroDivisionError:
+        pass
+
+    return dict(zip(COLUMNS, _odata))
+
+
+if __name__ == "__main__":
+    analyze_artices()
---
+++ imi/imi/models.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+import logging
+import os
+
+from sqlalchemy import (
+    create_engine, Column, inspect,
+    Integer, DateTime, Float, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import exc as sqlaException
+
+
+DB_USER = os.environ.get('DB_USER_IMI', 'postgres')
+DB_PASS = os.environ.get('DB_PASS_IMI', '')
+DB_HOST = os.environ.get('DB_HOST_IMI', '127.0.0.1')
+DB_PORT = os.environ.get('DB_PORT', 5432)
+DB_NAME = os.environ.get('DB_NAME_IMI', 'postgres')
+DB_SCHEMA = os.environ.get('DB_SCHEMA_IMI', 'imi')
+
+pg_config = {
+    'username': DB_USER,
+    'password': DB_PASS,
+    'host': DB_HOST,
+    'port': DB_PORT,
+    'database': DB_NAME,
+    'schema': DB_SCHEMA
+}
+
+logger = logging.getLogger(__name__)
+pg_dsn = "postgresql+psycopg2://{username}:{password}@{host}:5432/postgres".format(**pg_config)
+Base = declarative_base()
+db_engine = create_engine(
+    pg_dsn,
+    connect_args={"application_name": 'imi:' + str(__name__)},
+    pool_size=200,
+    pool_recycle=600,
+    max_overflow=0,
+    encoding='utf-8'
+    )
+try:
+    conn = db_engine.connect()
+    conn.execute("commit")
+    conn.execute("create database {}".format(pg_config['database']))
+    conn.close()
+except sqlaException.ProgrammingError as Exception:
+    pass
+
+
+class ImiTexts(Base):
+    __tablename__ = 'imi_texts'
+    __table_args__ = {"schema": pg_config['schema']}
+
+    gan = Column(Integer, primary_key=True)
+    text = Column(String)
+
+
+class Analysis(Base):
+    __tablename__ = 'analysis'
+    __table_args__ = {"schema": pg_config['schema']}
+
+    gan = Column(Integer, primary_key=True)
+    word_count = Column(Integer)
+    positive = Column(Float)
+    negative = Column(Float)
+    uncertainty = Column(Float)
+    litigious = Column(Float)
+    modal_weak = Column(Float)
+    modal_moderate = Column(Float)
+    modal_strong = Column(Float)
+    constraining = Column(Float)
+    alphabetic = Column(Integer)
+    digits = Column(Integer)
+    numbers = Column(Integer)
+    avg_syllables_per_word = Column(Float)
+    avg_word_length = Column(Float)
+    vocabulary = Column(Integer)
+    created = Column(DateTime, default=datetime.utcnow)
+
+    def _asdict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+
+class SentimentHarvard(Base):
+    __tablename__ = 'sentiment_harvard'
+    __table_args__ = {"schema": pg_config['schema']}
+
+    gan = Column(Integer, primary_key=True)
+    word_count = Column(Integer)
+    positiv = Column(Float)
+    negativ = Column(Float)
+    pstv = Column(Float)
+    affil = Column(Float)
+    ngtv = Column(Float)
+    hostile = Column(Float)
+    strong = Column(Float)
+    power = Column(Float)
+    weak = Column(Float)
+    submit = Column(Float)
+    active = Column(Float)
+    passive = Column(Float)
+    created = Column(DateTime, default=datetime.utcnow)
+
+
+Base.metadata.create_all(db_engine)
